41d22c0738fafec471802f50143b5f01806ecf62
root <root@k8-sm-cluster.novalocal>
2022-09-05 04:37:50 +0000

    update

--- common.mk
+++ common.mk
@@ -5,13 +5,5 @@ build: docker_build docker_push
 docker_build:
 	docker build --build-arg=http_proxy=${http_proxy} --build-arg=https_proxy=${http_proxy} -t $(REGISTRY)/$(PACKAGE):$(IMAGE_VERSION) .
 
-push: docker_push
-
 docker_push:
 	docker push ${REGISTRY}/$(PACKAGE):${IMAGE_VERSION}
-
-push_release:
-	docker push ${NEW_REGISTRY}/$(PACKAGE):${IMAGE_VERSION}
-
-retag:
-	docker tag ${REGISTRY}/${PACKAGE}:${IMAGE_VERSION}${IMAGE_TAG} ${NEW_REGISTRY}/${PACKAGE}:${IMAGE_VERSION}
